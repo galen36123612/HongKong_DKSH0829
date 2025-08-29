615cf61dc362e2ce5f6be32836dd63da49532dd6
galen36123612 <82554245+galen36123612@users.noreply.github.com>
2025-08-29 15:11:03 +0800

    Update and rename route.ts to route.ts

--- src/app/api/blob_test/route.ts
+++ src/app/api/blob_test/route.ts
@@ -1,5 +1,5 @@
 // 0811 src/app/api/blob-test/route.ts 測試
-import { put } from "@vercel/blob";
+//import { put } from "@vercel/blob";
 
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
